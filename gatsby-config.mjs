06cacc9cad84dbcb6c84dcef355dd90428c5fa36
Jake Bukuts <Jake Bukuts>
2023-04-10 14:01:08 -0400

    add to readme, side nav now expands only current page on inital load

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -79,7 +79,9 @@ const config = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'content',
-        path: `${__dirname}/content`
+        path: `${__dirname}/content`,
+        ignore: isDev ? [] : ['**/test.md'],
+        fastHash: true
   ***REMOVED***,
       __key: 'content'
 ***REMOVED***
